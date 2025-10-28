bc0d5f5b572f614841a693b59668e1b4649d895a
Dino Source <dinosource.cpp@gmail.com>
2025-10-28 06:05:37 -0700

    CRSP-8: Add `AccessRequirements` class
    We need choices for Course.access field, so as title says.

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
 
+class AccessRequirements(models.TextChoices):
+    ANYONE = "any", "Anyone"
+    EMAIL_REQUIRED = "email_requred", "Email required"
+
+
 class PublishStatus(models.TextChoices):
     PUBLISHED = "pub", "Publish"
     COMING_SOON = "soon", "Coming soon"
@@ -14,6 +19,11 @@ class Course(models.Model):
         max_length=10,
         choices=PublishStatus.choices,
         default=PublishStatus.DRAFT,
+    )
+    access = models.CharField(
+        max_length=10,
+        choices=AccessRequirements.choices,
+        default=AccessRequirements.ANYONE,
     )
 
     @property
