329f9b51593fc85b5434204668d575baa095cc35
Dino Source <dinosource.cpp@gmail.com>
2025-11-04 06:29:30 -0800

    Fix typo

--- src/courses/models.py
+++ src/courses/models.py
@@ -7,7 +7,7 @@ class AccessRequirements(models.TextChoices):
 
 
 class PublishStatus(models.TextChoices):
-    PUBLISHED = "pub", "Publish"
+    PUBLISHED = "pub", "Published"
     COMING_SOON = "soon", "Coming soon"
     DRAFT = "draft", "Draft"
 
