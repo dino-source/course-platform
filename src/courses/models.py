637da0c4ec0ec52d95b1f384b705a9b3b8fc48c9
Dino Source <dinosource.cpp@gmail.com>
2025-11-11 06:08:53 -0800

    CRSP-15: Use CloudinaryField

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,5 +1,6 @@
 import cfehome.helpers as helpers
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 
 helpers.cloudinary_init()
@@ -25,7 +26,7 @@ def handle_upload(instance, filename):
 class Course(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
-    image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
+    image = CloudinaryField("image", null=True)
     status = models.CharField(
         max_length=16,
         choices=PublishStatus.choices,
